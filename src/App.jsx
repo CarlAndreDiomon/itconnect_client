61789af8e15b87382751d373f739625d12045995
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-10-26 13:34:26 +0800

    Set up Environment

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landingPage';
+import HomePage from './pages/homePage';
+
+const App = () => {
+  return (
+    <div className=' flex h-screen'>
+      
+      <h1></h1>
+      <div  className="flex-1 p-4 overflow-auto">
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/landing" element={<LandingPage />} />
+        </Routes>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
