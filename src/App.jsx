8c95f76dfa37d40e99786f61fe001b972347da34
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-10-29 20:27:19 +0800

    App.jsx Version 1.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/layout/Navbar";
+import MobileNavbar from "./components/layout/mobile/MobileNavbar";
 import LoginPage from "./pages/loginPage";
+import DesktopSidebar from "./components/layout/desktop/DesktopSidebar";
 
 const App = () => {
   return (
-    <main className=" w-screen flex flex-col h-screen bg-gray-50">
-      <header className="h-14 bg-white border-b flex items-center justify-between ">
-        <Navbar />
-      </header>
+    <main className="w-screen h-screen flex bg-gray-50">
+      <aside className="hidden lg:flex w-64  bg-white">
+        <DesktopSidebar />
+      </aside>
 
-      <section className="flex-1 overflow-auto ">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </section>
+      <div className="flex-1 flex flex-col">
+        <header className="lg:hidden bg-white border-b">
+          <MobileNavbar />
+        </header>
+
+        <section className="flex-1 overflow-auto bg-gray-100">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </section>
+      </div>
     </main>
   );
 };
