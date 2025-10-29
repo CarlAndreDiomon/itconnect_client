824d37b9746f5f81d7cc32e2dd806024270050f4
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-10-29 19:13:32 +0800

    Mobile Navbar Version 1.0.0

---
+++ src/components/layout/mobile/MobileNavbar.jsx
@@ -0,0 +1,62 @@
+import {
+  BellIcon,
+  HomeIcon,
+  MessagesSquareIcon,
+  Settings2Icon,
+  UserCircleIcon,
+  Users2Icon,
+} from "lucide-react";
+import SearchBar from "../../ui/SearchBar";
+
+export default function Navbar() {
+  return (
+    <main>
+      <header className="w-screen md:px-16 h-full flex flex-col gap-3 p-4">
+        <nav className=" flex justify-between ">
+          <h1 className="font-bold text-3xl"> IT Connects </h1>
+
+          <ul className=" flex gap-3 items-center">
+            <li>
+              <SearchBar />
+            </li>
+            <li>
+              <a href="/settings">
+                <Settings2Icon />
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <nav>
+          <ul className="flex justify-between">
+            <li>
+              <a href="/">
+                <HomeIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/messages">
+                <MessagesSquareIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/friends">
+                <Users2Icon />
+              </a>
+            </li>
+            <li>
+              <a href="/profile">
+                <UserCircleIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/notification">
+                <BellIcon />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </main>
+  );
+}
